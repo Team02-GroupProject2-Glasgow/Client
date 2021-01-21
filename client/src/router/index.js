349eb3c0bdf6149c69d686d9f7a7039b531ce88e
pajarwitarsa <pajarwitarsa@gmail.com>
2021-01-21 15:42:24 +0700

    Add WelcomePage Views

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import WelcomePage from '../views/WelcomePage.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/welcome',
+    name: 'Welcome',
+    component: WelcomePage
   },
   {
     path: '/about',
