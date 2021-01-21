95c5b3e43cd61638df20dc264e27e02b69e67a02
ariftoteles <arif.santoso4@gmail.com>
2021-01-21 21:56:43 +0700

    Layout for game board is done

--- client/src/components/GameBoard.vue
+++ client/src/components/GameBoard.vue
@@ -1,78 +1,87 @@
 <template>
   <div class="game-board position-relative bg-1">
     <h2 class="text-white mb-5">Game Tebak Kata</h2>
-    <div class="progres position-absolute text-warning fs-1">
-      1/5
-    </div>
-    <div class="leader-board position-absolute text-white">
-      <span class="fs-4 fw-bold">LEADER BOARD</span>
-      <div class="bg-3 p-1 fs-5 rounded-pill mt-2">
-        <span class="mx-2">Abby</span>
-        <span class="mx-2 text-warning">4/5</span>
+    <button v-if="!start" @click="startGame" class="btn btn-success">Start</button>
+    <div v-if="start">
+      <div class="progres position-absolute text-warning fs-1">
+        {{ progress }}/5
       </div>
-      <div class="bg-3 p-1 fs-5 rounded-pill mt-2">
-        <span class="mx-2">Anthony</span>
-        <span class="mx-2 text-warning">3/5</span>
-      </div>
-      <div class="bg-3 p-1 fs-5 rounded-pill mt-2">
-        <span class="mx-2">Arif</span>
-        <span class="mx-2 text-warning">2/5</span>
-      </div>
-      <div class="bg-3 p-1 fs-5 rounded-pill mt-2">
-        <span class="mx-2">Pajar</span>
-        <span class="mx-2 text-warning">2/5</span>
-      </div>
-    </div>
-    <!-- Qlue -->
-    <div class="w-50 mx-auto text-white fs-5">
-      <div
-        v-for="(qlue, ind) in word.qlue"
-        :key="ind"
-        class="mb-3 rounded overflow-hidden"
-      >
-        <div class="d-flex justify-content-start">
-          <div class="col-2 bg-4 p-2">{{ ind + 1 }}</div>
-          <div class="col-10 bg-2 p-2 fw-bold">{{ qlue }}</div>
+      <!-- Leader Board -->
+      <!-- <div class="leader-board position-absolute text-white">
+        <span class="fs-4 fw-bold">LEADER BOARD</span>
+        <div class="bg-3 p-1 fs-5 rounded-pill mt-2">
+          <span class="mx-2">Abby</span>
+          <span class="mx-2 text-warning">4/5</span>
+        </div>
+        <div class="bg-3 p-1 fs-5 rounded-pill mt-2">
+          <span class="mx-2">Anthony</span>
+          <span class="mx-2 text-warning">3/5</span>
+        </div>
+        <div class="bg-3 p-1 fs-5 rounded-pill mt-2">
+          <span class="mx-2">Arif</span>
+          <span class="mx-2 text-warning">2/5</span>
+        </div>
+        <div class="bg-3 p-1 fs-5 rounded-pill mt-2">
+          <span class="mx-2">Pajar</span>
+          <span class="mx-2 text-warning">2/5</span>
+        </div>
+      </div> -->
+      <!-- Qlue -->
+      <div class="w-50 mx-auto text-white fs-5">
+        <div
+          v-for="(clue, ind) in word.clue"
+          :key="ind"
+          class="mb-3 rounded overflow-hidden"
+        >
+          <div class="d-flex justify-content-start">
+            <div class="col-2 bg-4 p-2">{{ ind + 1 }}</div>
+            <div class="col-10 bg-2 p-2 fw-bold">{{ clue }}</div>
+          </div>
         </div>
       </div>
-    </div>
-    <!-- Answer -->
-    <div class="d-flex justify-content-between w-50 mt-5 mx-auto">
-      <div class="col-2 mx-auto bg-2 p-3 rounded">
-        <input v-model="first" type="text" class="form-control text-center fs-4 fw-bold text-uppercase" maxlength="1">
+      <!-- Answer -->
+      <div class="d-flex justify-content-between w-50 mt-5 mx-auto">
+        <div class="col-2 mx-auto bg-2 p-3 rounded">
+          <input v-model="first" type="text" class="form-control text-center fs-4 fw-bold text-uppercase" maxlength="1">
+        </div>
+        <div class="col-2 mx-auto bg-2 p-3 rounded">
+          <input v-model="second" type="text" class="form-control text-center fs-4 fw-bold text-uppercase" maxlength="1">
+        </div>
+        <div class="col-2 mx-auto bg-2 p-3 rounded">
+          <input v-model="third" type="text" class="form-control text-center fs-4 fw-bold text-uppercase" maxlength="1">
+        </div>
+        <div class="col-2 mx-auto bg-2 p-3 rounded">
+          <input v-model="fourth" type="text" class="form-control text-center fs-4 fw-bold text-uppercase" maxlength="1">
+        </div>
+        <div class="col-2 mx-auto bg-2 p-3 rounded">
+          <input v-model="fifth" type="text" class="form-control text-center fs-4 fw-bold text-uppercase" maxlength="1">
+        </div>
       </div>
-      <div class="col-2 mx-auto bg-2 p-3 rounded">
-        <input v-model="second" type="text" class="form-control text-center fs-4 fw-bold text-uppercase" maxlength="1">
-      </div>
-      <div class="col-2 mx-auto bg-2 p-3 rounded">
-        <input v-model="third" type="text" class="form-control text-center fs-4 fw-bold text-uppercase" maxlength="1">
-      </div>
-      <div class="col-2 mx-auto bg-2 p-3 rounded">
-        <input v-model="fourth" type="text" class="form-control text-center fs-4 fw-bold text-uppercase" maxlength="1">
-      </div>
-      <div class="col-2 mx-auto bg-2 p-3 rounded">
-        <input v-model="fifth" type="text" class="form-control text-center fs-4 fw-bold text-uppercase" maxlength="1">
+      <!-- Button Next / Wrong -->
+      <div v-if="isTrue" class="mt-3">
+        <button @click="nextGame(word.id)" class="btn btn-success w-50">Next Question</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   name: 'GameBoard',
   data () {
     return {
-      word: {
-        id: 1,
-        qlue: ['nyamuk', 'dinding', 'blabala', 'bdjasd', 'kjdsad'],
-        answer: 'CICAK'
-      },
+      start: false,
+      progress: 0,
+      word: {},
       first: '',
       second: '',
       third: '',
       fourth: '',
       fifth: '',
-      answer: ''
+      answer: '',
+      isTrue: false
     }
   },
   watch: {
@@ -93,12 +102,36 @@ export default {
     }
   },
   methods: {
+    startGame () {
+      this.start = true
+      this.word = this.words[0]
+      console.log(this.word)
+    },
     checkAnswer () {
-      this.answer = `${this.first}${this.second}${this.third}${this.fourth}${this.fifth}`.toUpperCase()
+      this.answer = `${this.first}${this.second}${this.third}${this.fourth}${this.fifth}`
       if (this.answer === this.word.answer) {
         console.log('benar')
+        // membuat event ke server
+        this.isTrue = true
+      } else if (this.answer.length === 5) {
+        console.log('salah')
       }
+    },
+    nextGame (id) {
+      this.isTrue = false
+      this.progress++
+      this.first = ''
+      this.second = ''
+      this.third = ''
+      this.fourth = ''
+      this.fifth = ''
+      this.word = this.words[id]
     }
+  },
+  computed: {
+    ...mapState({
+      words: 'words'
+    })
   }
 }
 </script>
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,10 +5,18 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    words: []
   },
   mutations: {
+    setWords (state, payload) {
+      state.words = payload
+    }
   },
   actions: {
+    SOCKET_init (context, payload) {
+      console.log('Init bro')
+      context.commit('setWords', payload)
+    }
   },
   modules: {
   }
